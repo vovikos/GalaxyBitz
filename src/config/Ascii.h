51df94f1587aff0e6a646385e37158143e34bbd8
nox <noxxnetwork@gmail.com>
2019-01-02 12:28:08 +0100

    say hello

---
+++ src/config/Ascii.h
@@ -0,0 +1,15 @@
+// Copyright (c) 2018, The TurtleCoin Developers
+// Copyright (c) 2019, GalaxyBitz Project
+//
+// Please see the included LICENSE file for more information
+
+#pragma once
+
+const std::string asciiArt =
+      "\n                                   \n"
+        "██████╗ █████╗ █████╗ ██╗███╗   ██╗\n"
+        "██╔═══╝██╔═══╝██╔══██╗██║████╗  ██║\n"
+        "██ ███╗██║    ██║  ██║██║██╔██╗ ██║\n"
+        "██╔═██║██║    ██║  ██║██║██║╚██╗██║\n"
+        "██████║██████╗╚█████╔ ██║██║ ╚████║\n"
+        "╚═════╝╚════╝  ╚════╝ ╚═╝╚═╝  ╚═══╝\n";
